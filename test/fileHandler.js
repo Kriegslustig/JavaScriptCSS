dc3f04f6af99219ae9452a863cc8b036a2fbc10c
Kriegslustig <github@ls7.ch>
2015-05-15 23:27:06 +0200

    No moah async in the tests

--- test/fileHandler.js
+++ test/fileHandler.js
@@ -1,23 +1,20 @@
 var assert = require('assert')
-var fileHandler = require('fileHandler')
+var fileHandler = require('../lib/fileHandler')
 var fs = require('fs')
 
 describe('fileHandler', function () {
   var testFile = './test.txt'
   var testString = 'asdf'
   describe('read', function () {
-    fs.open(testFile, 'w', function (err, fd) {
-      fs.write(fd, testString, function (err) {
-        it('should return the contents of a file as a string', function () {
-          assert.equal(testString, fileHandler.read(testFile))
-        })
-        it('should return false if the file doesn\'t exsist', function () {
-          assert.equal(false, fileHandler.read('./gabeldigu'))
-        })
-        fs.close(fd, function () {
-          fs.unlink(testFile)
-        })
-      })
+    var fileStream = fs.openSync(testFile, 'w')
+    fs.writeSync(fileStream, testString)
+    it('should return the contents of a file as a string', function () {
+      assert.equal(testString, fileHandler.read(testFile))
     })
+    it('should return false if the file doesn\'t exsist', function () {
+      assert.equal(false, fileHandler.read('./gabeldigu'))
+    })
+    fs.closeSync(fileStream)
+    fs.unlink(testFile)
   })
 })
