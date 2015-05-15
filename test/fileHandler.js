8e43a6dcb8bd6081e05be325933d1b112a548800
Kriegslustig <github@ls7.ch>
2015-05-15 23:57:02 +0200

    Write failing tests for fileHandler.write

--- test/fileHandler.js
+++ test/fileHandler.js
@@ -5,6 +5,7 @@ var fs = require('fs')
 describe('fileHandler', function () {
   var testFile = './test.txt'
   var testString = 'asdf'
+
   describe('read', function () {
     var fileStream = fs.openSync(testFile, 'w')
     fs.writeSync(fileStream, testString)
@@ -16,7 +17,23 @@ describe('fileHandler', function () {
       assert.equal(false, fileHandler.read('./gabeldigu'))
     })
     after(function () {
-      console.log('removing file')
+      fs.unlink(testFile)
+    })
+  })
+
+  describe('write', function () {
+    fileHandler.write(testFile, testString)
+    it('Should create the file if it doesn\'t exsist', function () {
+      assert.equal(true, fs.existsSync(testFile))
+    })
+    it('Should write the first argument to the file', function () {
+      assert.equal(testString, fs.readFileSync(testFile, {encoding: 'utf8'}))
+    })
+    it('Should overwrite a file if it already exists', function () {
+      fileHandler.write(testFile, 1 + testString + 1)
+      assert.equal(1 + testFile + 1, fs.readFileSync(testFile, {encoding: 'utf8'}))
+    })
+    after(function () {
       fs.unlink(testFile)
     })
   })
