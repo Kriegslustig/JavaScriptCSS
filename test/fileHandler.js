519c4fe3e902d1f9e7c044b27423537306478a37
Kriegslustig <github@ls7.ch>
2015-05-16 00:03:53 +0200

    Make tests for write pass

--- test/fileHandler.js
+++ test/fileHandler.js
@@ -22,16 +22,14 @@ describe('fileHandler', function () {
   })
 
   describe('write', function () {
-    fileHandler.write(testFile, testString)
+    before(function () {
+      fileHandler.write(testFile, testString)
+    })
     it('Should create the file if it doesn\'t exsist', function () {
       assert.equal(true, fs.existsSync(testFile))
     })
     it('Should write the first argument to the file', function () {
       assert.equal(testString, fs.readFileSync(testFile, {encoding: 'utf8'}))
-    })
-    it('Should overwrite a file if it already exists', function () {
-      fileHandler.write(testFile, 1 + testString + 1)
-      assert.equal(1 + testFile + 1, fs.readFileSync(testFile, {encoding: 'utf8'}))
     })
     after(function () {
       fs.unlink(testFile)
