63baf86fb8997b962b8863a7e7ef56e06827643a
Kriegslustig <github@ls7.ch>
2015-05-16 15:34:40 +0200

    Write some tests for parser.parse

--- test/parser.js
+++ test/parser.js
@@ -4,7 +4,32 @@ var parser = require('../lib/parser.js')
 
 describe('parser', function () {
   describe('parse', function () {})
-  describe('splitJS', function () {})
+  describe('splitJS', function () {
+    var testJcss = '\
+      div {\
+        display: block;\
+      }\
+      css(".class { display: none; }")\
+    '
+    var testCss = {css:  '\
+      div {\
+        display: block;\
+      }',
+      js:'\
+      css(".class { display: none; }")\
+    '}
+    it('should return an array of arrays with the inner array having js or css in the first position and a string at the second', function () {
+      var parseReturn = parser.splitJS(testJcss)
+      assert.equal('object', typeof parseReturn)
+      assert.equal(2, parseReturn[0].length)
+      assert.ok(parseReturn[0][0] == 'js' || parseReturn[0][0] == 'css')
+      assert.equal('string', typeof parseReturn[0][1])
+    })
+    it('should interpret line endings before and after css as css', function () {
+      assert.equal('\n', parseReturn[0][1][0])
+      assert.equal('\n', parseReturn[0][1][parseReturn[0][1].length - 1])
+    })
+  })
   describe('exec', function () {
     it('should return a string', function () {
       assert.equal('string', typeof parser.exec(''))
