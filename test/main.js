9cb75e62aba61b04005c220eebc55e8441664850
Kriegslustig <github@ls7.ch>
2015-05-17 13:46:43 +0200

    Write some tests for main

---
+++ test/main.js
@@ -0,0 +1,28 @@
+var assert = require('assert')
+var fs = require('fs')
+
+var main = require('../jcss.js')
+
+describe('main', function () {
+  it('should take require at least one arguments', function () {
+    assert.equal(1, main())
+  })
+  it('should throw an error if the first argument is not a file', function () {
+    assert.equal(1, main('notafile'))
+  })
+  it('should create the file passed as the second argument.', function () {
+    var testFileName = 'testFile'
+    var testString = 'div {display: block;}'
+    function deleteTestFiles () {
+      fs.unlink(testFileName + '.css')
+      fs.unlink(testFileName + '.jcss')
+    }
+    fs.writeFile(testFile + '.jcss', testString, function () {
+      main(testFile + '.jcss')
+      fs.readdir('./', function (err, dir) {
+        assert.ok(dir.indexOf(testFile + '.css') > -1)
+        deleteTestFiles()
+      })
+    })
+  })
+})
