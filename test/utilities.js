3dd3ac179d0223b4b3d20f5d8ebf66d7b5f76a0b
Kriegslustig <github@ls7.ch>
2015-05-19 15:48:26 +0200

    Add tests for utilities.caller

---
+++ test/utilities.js
@@ -0,0 +1,15 @@
+var utilities = require('../lib/utilities.js')
+
+describe('utilities', function () {
+  describe('caller', function () {
+    it('should take a string as a first argument', function () {
+      assert.ok(utilities.caller('0'))
+    })
+    it('should return a function', function () {
+      assert.equal('function', typeof utilities.caller('0'))
+    })
+    it('should return a function that calles a function on its argument, the function name is passed to caller', function () {
+      assert.ok(utilities.caller('0')([function () {return true}]))
+    })
+  })
+})
