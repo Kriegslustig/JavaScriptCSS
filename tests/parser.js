d690131d87017af00a3ae0f9d6012076bd850ec4
Kriegslustig <github@ls7.ch>
2015-05-21 22:17:27 +0200

    Move back to prototypial

--- tests/parser.js
+++ tests/parser.js
@@ -6,26 +6,31 @@ var Parser = require('../lib/parser.js')
 describe('parser', function () {
   describe('parse', function () {
     it('should return a string', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('string', typeof testParser.parse(''))
     })
     it('should return false if there\'s invalid any javascript in the jsheets', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.ok(!testParser.parse('asdf'))
     })
     it('should take jsheets and return css', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('asdf', testParser.parse('["a", "s", "d", "f"].forEach(function (value) { css(value) })'))
       assert.equal('div {\n  display: block;\n}\ntroll', testParser.parse('div {\n  display: block;\n}\n  css(\'troll\')'))
       assert.equal('div {\n          display: block;\n        }\ntrolltrolltrolltrolltrolltrolltrolltrolltrolltroll\n        div { display: hidden; }', testParser.parse('div {\n          display: block;\n        }\n        \n        for(var i = 10; i > 0; i--) {\n          css(\'troll\')\n        }\n\n        div { display: hidden; }'))
     })
     it('should return false if false is passed to it', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal(false, testParser.parse(false))
     })
 
     it('should be able to set, use and reset variables', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var testString
       testParser.parse('$.someString = \'someString\'')
       assert.equal('someString', testParser.context['$']['someString'])
@@ -36,12 +41,14 @@ describe('parser', function () {
     })
 
     it('Should keep variables in global space', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('a{...}\ntrue', testParser.parse('var testVar = true \na{...}\ncss(testVar)'))
     })
 
     it('should run the onDone hook', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       testParser.context.onDone = [function () {css('css')}]
       assert.equal('\ncss', testParser.parse(''))
     })
@@ -49,11 +56,13 @@ describe('parser', function () {
 
   describe('interpret', function () {
     it('should exec js if the first position is js', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('asdf', testParser.interpret(['js', '["a", "s", "d", "f"].forEach(function (value) { css(value) })']))
     })
     it('should return the passed string if the first position is css', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('display: block', testParser.interpret(['css', 'display: block']))
     })
   })
@@ -63,7 +72,8 @@ describe('parser', function () {
     var testCss
     var parseReturn
     before(function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       testJcss = '\n        div {\n          display: block;\n        }\n\n        css(".class { display: none; }")\n      '
       testCss = {css:  '\n        div {\n          display: block;\n        }',
         js:'\n        css(".class { display: none; }")\n      '}
@@ -82,30 +92,35 @@ describe('parser', function () {
       assert.equal('\n', parseReturn[1][parseReturn[1].length - 2])
     })
     it('should be able to handle only js or only css', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('css("div{display:block;}")', testParser.split('css("div{display:block;}")')[1])
       assert.equal('div{display:block;}', testParser.split('div{display:block;}')[1])
     })
     it('should return false if the passed string is empty', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal(false, testParser.split(''))
     })
     it('should mark css and js as such', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('div {\n          display: block;\n        }\n',
         testParser.split('div {\n          display: block;\n        }\n        \n        for(var i = 10; i < 0; i--) {\n          css(\'troll\')\n        }\n        ')[1])
       assert.equal('        \n        for(var i = 10; i < 0; i--) {\n          css(\'troll\')\n        }\n        ',
         testParser.split('        \n        for(var i = 10; i < 0; i--) {\n          css(\'troll\')\n        }\n        ')[1])
     })
     it('should return the rest as the third position and false if there is nothing left', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('        \n        for(var i = 10; i < 0; i--) {\n          css(\'troll\')\n        }\n        ',
         testParser.split('div {\n          display: block;\n        }\n        \n        for(var i = 10; i < 0; i--) {\n          css(\'troll\')\n        }\n        ')[2])
       assert.equal(false,
         testParser.split('div {\n          display: block;\n        }')[2])
     })
     it('should know all css selectors', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var testCss = '{display: block;}'
       assert.equal('css', testParser.split('*' + testCss)[0])
       assert.equal('css', testParser.split('E' + testCss)[0])
@@ -152,7 +167,8 @@ describe('parser', function () {
     })
 
     it('should be able to recognize mediaqueries as such', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
         assert.equal('css', testParser.split('@media all and (min-width:500px) { … }')[0])
         assert.equal('css', testParser.split('@media (min-width:500px) { … }')[0])
         assert.equal('css', testParser.split('@media (orientation: portrait) { … }')[0])
@@ -164,7 +180,8 @@ describe('parser', function () {
     })
 
     it('should recognize all js as not css', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('js', testParser.split('if(true) {return false}')[0])
       assert.equal('js', testParser.split('if(x = y) return something')[0])
       assert.equal('js', testParser.split('while(someFun()) {i++}')[0])
@@ -178,27 +195,32 @@ describe('parser', function () {
 
   describe('parseCssVars', function () {
     it('should take a string and return one', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('string', typeof testParser.parseCssVars(''))
     })
     it('should replace variables starting with `$.` with its values', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       testParser.context.$ = { s: 'something' }
       assert.equal('something', testParser.parseCssVars('$.s'))
     })
     it('should replace function calls starting with `$.` with its return values', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var someString = 'someString'
       testParser.context.$ = { f: function () { return someString } }
       assert.equal(someString, testParser.parseCssVars('$.f()'))
     })
     it('should be able to handle arguments passed to functions', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       testParser.context.$ = { add: function (n0, n1) { return n0 + n1 } }
       assert.equal(2, testParser.parseCssVars('$.add(1,1)'))
     })
     it('should be able to pick out vars in the middle of css', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       testParser.context.$ = { str: 'block' }
       assert.equal('div { display: block; }', testParser.parseCssVars('div { display: $.str; }'))
     })
@@ -206,81 +228,96 @@ describe('parser', function () {
 
   describe('splitAroundSubstring', function () {
     it('should take two strings as arguments', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.ok(testParser.splitAroundSubstring(' 1', ' 1 '))
     })
     it('should return an array of two strings', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('object', typeof testParser.splitAroundSubstring(' 1', ' 1 '))
       assert.equal(2, testParser.splitAroundSubstring(' 1', ' 1 ').length)
       assert.equal('string', typeof testParser.splitAroundSubstring(' 1', ' 1 ')[0])
       assert.equal('string', typeof testParser.splitAroundSubstring(' 1', ' 1 ')[1])
     })
     it('should return two substrings of the second', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var s1 = ' 1'
       var s2 = ' 1 '
       assert.ok(s2.indexOf(testParser.splitAroundSubstring(s1, s2)[0]) > -1)
       assert.ok(s2.indexOf(testParser.splitAroundSubstring(s1, s2)[1]) > -1)
     })
     it('should return everything before the passed substring as the first return value if there is anything', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal(' ', testParser.splitAroundSubstring('1 ', ' 1 ')[0])
     })
     it('should return a string including the substring as the second return value if the substring isn\'t at the start of the string', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('1 -', testParser.splitAroundSubstring('1 ', ' 1 -')[1])
     })
   })
 
   describe('exec', function () {
     it('should return a string', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('string', typeof testParser.exec(''))
     })
     it('should return false if the string that is passed isn\'t valid JS.', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal(false, testParser.exec('yourmum'))
     })
     it('should provide a function `css` to the JS beeing execute', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('display: block', testParser.exec('css("display: block")'))
     })
     it('should exec JS', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('asdf', testParser.exec('["a", "s", "d", "f"].forEach(function (value) { css(value) })'))
     })
   })
 
   describe('funcsToString', function () {
     it('should take an array of functions as an argument', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.ok(testParser.funcsToString([function a () {}]))
     })
     it('should return a string', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('string', typeof testParser.funcsToString([function a () {}]))
     })
     it('should join the functions with newlines', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.ok(testParser.funcsToString([function a () {}, function a () {}]).indexOf('\n') > -1)
     })
     it('should add a newline in to the beginning of the string', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       assert.equal('\n', testParser.funcsToString([function a () {}])[0])
     })
   })
 
   describe('include', function () {
     it('should read and parse a jsheets file', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var testFile = './testfile.jsheet'
       fs.writeFileSync(testFile, 'css(\'div{display: block}\')')
       assert.equal('div{display: block}', testParser.include(testFile))
       fs.unlink(testFile)
     })
     it('should log an error and return an empty string if the jsheets is invalid or if the file doesn\'t exsist', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var testFile = './testfile.jsheet'
       assert.equal('', testParser.include('asddgere.cevrscs'))
       fs.writeFileSync(testFile, 'css(\'div{display: block}\')yoomum')
@@ -292,7 +329,8 @@ describe('parser', function () {
 
   describe('makeIncludes', function () {
     it('should replace calles to include in parsed jsheets files', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       var testFile = './testfile.jsheet'
       fs.writeFileSync(testFile, 'css(\'div{display: block}\')')
       assert.equal('div{display: block}', testParser.makeIncludes('include(\'' + testFile + '\')'))
@@ -302,12 +340,12 @@ describe('parser', function () {
 
   describe('initialize', function () {
     it('should write the context as this\'s ownProperty', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
       testParser.initialize()
       assert.ok(!testParser.hasOwnProperty(context))
     })
     it('should overwrite the context with a new one', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
       testParser.context.somevar = ''
       testParser.initialize()
       assert.ok(!testParser.context.somevar)
@@ -316,7 +354,8 @@ describe('parser', function () {
 
   describe('cleanContext', function () {
     it('should clear the returnString', function () {
-      var testParser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
       testParser.context.returnString = 'css'
       testParser.cleanContext()
       assert.equal('', testParser.context.returnString)
@@ -325,8 +364,10 @@ describe('parser', function () {
 
   describe('mergeContext', function () {
     it('should merge the passed context with this.context', function () {
-      var testParser = new Parser
-      var test2Parser = new Parser
+      var testParser = Object.create(Parser)
+      testParser.initialize()
+      var test2Parser = Object.create(Parser)
+      test2Parser.initialize()
       testParser.context.somevar = true
       testParser.mergeContext(test2Parser.context)
       assert.ok(testParser.context.somevar)
