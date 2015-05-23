395a258366a6ed13b9133aac339aea9223adb4cb
Kriegslustig <github@ls7.ch>
2015-05-23 18:34:25 +0200

    Make helpers an instanciable object for it to be able to access the context

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,5 +1,28 @@
-module.exports = {
-  $: function (expression) {
-    return expression
+module.exports = function (context) {
+  return {
+    $: function (expression) {
+      return expression
+    },
+
+    extend: {
+      extendables: {},
+      add: function (selector, cssString) {
+        this.extendables[selector] = {
+          css: cssString,
+          selectors: []
+        }
+      },
+      that: function (extendable, selector) {
+        if(!this.extendables[extendable]) return false
+        this.extendables[extendable]['selectors'].push(selector)
+        context.onDone.push(this.extendMaker(extendable))
+      },
+      extendMaker: function (extendable) {
+        var self = this
+        return function () {
+          return [self.extendables[extendable].selectors].concat(self.extendables[extendable].selectors).join(', ') + ' {' + self.extendables[extendable].css + '}\n'
+        }
+      }
+    }
   }
 }
--- tests/helpers.js
+++ tests/helpers.js
@@ -1,26 +1,62 @@
 var assert = require('assert')
-var helpers = require('../helpers/helpers.js')
+var vm = require('vm')
+
+var Helpers = require('../helpers/helpers.js')
 
 describe('helpers', function () {
   describe('$', function () {
+    var helpers = new Helpers({})
     it('should be able to parse an expression and return the pased value', function () {
       assert.equal(true, helpers.$(true))
     })
   })
 
   describe('extend', function () {
-    it('should be an object')
-    it('should have a function add')
-    it('should have a function that')
+    var context = {onDone: []}
+    var helpers = new Helpers(context)
+    it('should be an object', function () {
+      assert.equal('object', typeof helpers.extend)
+    })
+    it('should have a function add', function () {
+      assert.equal('function', typeof helpers.extend.add)
+    })
+    it('should have a function that', function () {
+      assert.equal('function', typeof helpers.extend.that)
+    })
     describe('add', function () {
-      it('should take two strings as arguments')
-      it('should take two strings as arguments')
-      it('should add functions to extend.extendables')
+      it('should take two strings as arguments', function () {
+        assert.equal(undefined, helpers.extend.add(' ', 'css'))
+      })
+      it('should add objects to extend.extendables', function () {
+        assert.equal('object', typeof helpers.extend.extendables[' ']);
+      })
     })
     describe('that', function () {
-      it('should only take strings for which there have been extendables added for as an argument')
-      it('should add a hook to onDone')
+      it('should take two arguments', function () {
+        assert.equal(undefined, helpers.extend.that(' ', ' '))
+      })
+      it('should only take strings for which there have been extendables added for as an argument', function () {
+        assert.ok(helpers.extend.that('!', '') !== undefined)
+        console.log(context)
+      })
+      it('should add a function to onDone', function () {
+        onDone = []
+        assert.equal(undefined, helpers.extend.that(' ', 'div'))
+        assert.equal('function', typeof context.onDone[0])
+      })
     })
-    it('should add css blocks to the end of a file')
+    describe('extendMaker', function () {
+      it('should return a function', function () {
+        assert.equal('function', typeof helpers.extend.extendMaker(' '))
+      })
+      it('should return a function that returns a string', function () {
+        assert.equal('string', typeof helpers.extend.extendMaker(' ')())
+      })
+    })
+    after(function () {
+      it('should add css blocks to the end of a file', function () {
+        assert.equal(' , div {css}\n', context.onDone[0]())
+      })
+    })
   })
 })
